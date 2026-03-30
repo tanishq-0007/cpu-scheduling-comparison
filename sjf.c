74018d6bcca0418c177cb384a70566f888cc5c83
Tanishq Singh <96950310+tanishq-0007@users.noreply.github.com>
2026-03-30 23:00:55 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,55 +1,65 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "scheduler.h"
 
-void sjf(int n, int at[], int bt[], float *avg_wt)
-{
-    int ct[n], tat[n], wt[n];
+void sjf(int n, int at[], int bt[], float *avg_wt, float *avg_tat){
+
+    int wt[n], tat[n];
     int completed[n];
 
     for(int i=0;i<n;i++)
-        completed[i] = 0;
+        completed[i]=0;
 
     int time = 0, done = 0;
+
     float total_wt = 0;
+    float total_tat = 0;
 
-    printf("\n--- SJF ---\nGantt Chart:\n");
+    printf("Gantt Chart:\n");
 
-    while(done < n)
-    {
-        int shortest = -1;
+    while(done < n){
 
-        for(int i=0;i<n;i++)
-        {
-            if(at[i] <= time && completed[i]==0)
-            {
-                if(shortest == -1 || bt[i] < bt[shortest])
-                    shortest = i;
+        int min_bt = 9999;
+        int index = -1;
+
+        for(int i=0;i<n;i++){
+            if(at[i] <= time && completed[i]==0){
+                if(bt[i] < min_bt){
+                    min_bt = bt[i];
+                    index = i;
+                }
             }
         }
 
-        if(shortest == -1)
-        {
-            time++;
-            continue;
+        if(index != -1){
+
+            wt[index] = time - at[index];
+
+            if(wt[index] < 0)
+                wt[index] = 0;
+
+            printf("%d | P%d | ", time, index+1);
+
+            time += bt[index];
+
+            tat[index] = wt[index] + bt[index];
+
+            total_wt += wt[index];
+            total_tat += tat[index];
+
+            completed[index] = 1;
+
+            done++;
         }
-
-        printf("%d | P%d | ",time,shortest+1);
-
-        time += bt[shortest];
-
-        ct[shortest] = time;
-        tat[shortest] = ct[shortest] - at[shortest];
-        wt[shortest] = tat[shortest] - bt[shortest];
-
-        total_wt += wt[shortest];
-
-        completed[shortest] = 1;
-        done++;
+        else{
+            time++;
+        }
     }
 
-    printf("%d",time);
+    printf("%d\n", time);
 
-    *avg_wt = total_wt/n;
+    *avg_wt = total_wt / n;
+    *avg_tat = total_tat / n;
 
-    printf("\nAverage Waiting Time = %.2f\n",*avg_wt);
+    printf("Average Waiting Time = %.2f\n", *avg_wt);
+    printf("Average Turnaround Time = %.2f\n", *avg_tat);
 }
