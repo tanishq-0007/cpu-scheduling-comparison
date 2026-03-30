aa72c637093a536d8ff906bb79b04cfad6ac3f10
Tanishq Singh <96950310+tanishq-0007@users.noreply.github.com>
2026-03-30 22:59:42 +0530

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,3 +1,8 @@
-void fcfs(int n, int at[], int bt[], float *avg_wt);
-void sjf(int n, int at[], int bt[], float *avg_wt);
-void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt);
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+void fcfs(int n, int at[], int bt[], float *avg_wt, float *avg_tat);
+void sjf(int n, int at[], int bt[], float *avg_wt, float *avg_tat);
+void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt, float *avg_tat);
+
+#endif
