3af3da4c7866281497a1c3f80aaff3b48874a4f6
angelsachdeva3560-cmd <angelsachdeva3560@gmail.com>
2026-03-30 22:30:31 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,40 +1,54 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "scheduler.h"
 
-void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt) {
-    int rt[n], wt[n], time = 0, completed = 0;
+void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt)
+{
+    int remaining[n];
+    int ct[n], tat[n], wt[n];
 
-    for (int i = 0; i < n; i++) {
-        rt[i] = bt[i];
-        wt[i] = 0;
-    }
+    for(int i=0;i<n;i++)
+        remaining[i] = bt[i];
 
-    while (completed < n) {
-        int done = 1;
-
-        for (int i = 0; i < n; i++) {
-            if (rt[i] > 0 && at[i] <= time) {
-                done = 0;
-
-                if (rt[i] > quantum) {
-                    time += quantum;
-                    rt[i] -= quantum;
-                } else {
-                    time += rt[i];
-                    wt[i] = time - bt[i] - at[i];
-                    rt[i] = 0;
-                    completed++;
-                }
-            }
-        }
-
-        if (done)
-            time++;
-    }
+    int time = 0;
+    int completed = 0;
 
     float total_wt = 0;
-    for (int i = 0; i < n; i++)
-        total_wt += wt[i];
+
+    printf("\n--- Round Robin ---\nGantt Chart:\n");
+
+    printf("0 ");
+
+    while(completed < n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(remaining[i] > 0)
+            {
+                if(remaining[i] > quantum)
+                {
+                    time += quantum;
+                    remaining[i] -= quantum;
+                }
+                else
+                {
+                    time += remaining[i];
+                    remaining[i] = 0;
+
+                    ct[i] = time;
+                    tat[i] = ct[i] - at[i];
+                    wt[i] = tat[i] - bt[i];
+
+                    total_wt += wt[i];
+
+                    completed++;
+                }
+
+                printf("| P%d | %d ",i+1,time);
+            }
+        }
+    }
 
     *avg_wt = total_wt / n;
+
+    printf("\nAverage Waiting Time = %.2f\n",*avg_wt);
 }
