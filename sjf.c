c20708e4a4726a755283ff001abf0aad0d9f497a
Tanishq Singh <96950310+tanishq-0007@users.noreply.github.com>
2026-03-30 22:20:16 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -4,52 +4,52 @@
 void sjf(int n, int at[], int bt[], float *avg_wt)
 {
     int ct[n], tat[n], wt[n];
+    int completed[n];
 
-    // simple sorting by burst time
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            if(bt[i] > bt[j])
-            {
-                int temp = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp;
+    for(int i=0;i<n;i++)
+        completed[i] = 0;
 
-                temp = at[i];
-                at[i] = at[j];
-                at[j] = temp;
-            }
-        }
-    }
-
-    ct[0] = at[0] + bt[0];
-
-    for(int i=1;i<n;i++)
-    {
-        if(ct[i-1] < at[i])
-            ct[i] = at[i] + bt[i];
-        else
-            ct[i] = ct[i-1] + bt[i];
-    }
-
+    int time = 0, done = 0;
     float total_wt = 0;
 
     printf("\n--- SJF ---\nGantt Chart:\n");
 
-    printf("%d ", at[0]);
-
-    for(int i=0;i<n;i++)
+    while(done < n)
     {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
+        int shortest = -1;
 
-        total_wt += wt[i];
+        for(int i=0;i<n;i++)
+        {
+            if(at[i] <= time && completed[i]==0)
+            {
+                if(shortest == -1 || bt[i] < bt[shortest])
+                    shortest = i;
+            }
+        }
 
-        printf("| P%d | %d ",i+1,ct[i]);
+        if(shortest == -1)
+        {
+            time++;
+            continue;
+        }
+
+        printf("%d | P%d | ",time,shortest+1);
+
+        time += bt[shortest];
+
+        ct[shortest] = time;
+        tat[shortest] = ct[shortest] - at[shortest];
+        wt[shortest] = tat[shortest] - bt[shortest];
+
+        total_wt += wt[shortest];
+
+        completed[shortest] = 1;
+        done++;
     }
 
-    *avg_wt = total_wt / n;
+    printf("%d",time);
+
+    *avg_wt = total_wt/n;
 
     printf("\nAverage Waiting Time = %.2f\n",*avg_wt);
 }
