643a4b9ea8fd335b004460637a92b1ccefc3561f
angelsachdeva3560-cmd <angelsachdeva3560@gmail.com>
2026-03-30 23:02:50 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,54 +1,68 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "scheduler.h"
 
-void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt)
-{
-    int remaining[n];
-    int ct[n], tat[n], wt[n];
+void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt, float *avg_tat){
 
-    for(int i=0;i<n;i++)
-        remaining[i] = bt[i];
+    int rem_bt[n], wt[n], tat[n];
+
+    for(int i=0;i<n;i++){
+        rem_bt[i] = bt[i];
+        wt[i] = 0;
+    }
 
     int time = 0;
-    int completed = 0;
 
-    float total_wt = 0;
+    printf("Gantt Chart:\n0 ");
 
-    printf("\n--- Round Robin ---\nGantt Chart:\n");
+    int done;
 
-    printf("0 ");
+    do{
 
-    while(completed < n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            if(remaining[i] > 0)
-            {
-                if(remaining[i] > quantum)
-                {
+        done = 1;
+
+        for(int i=0;i<n;i++){
+
+            if(rem_bt[i] > 0){
+
+                done = 0;
+
+                printf("| P%d | ", i+1);
+
+                if(rem_bt[i] > quantum){
+
                     time += quantum;
-                    remaining[i] -= quantum;
+
+                    rem_bt[i] -= quantum;
                 }
-                else
-                {
-                    time += remaining[i];
-                    remaining[i] = 0;
+                else{
 
-                    ct[i] = time;
-                    tat[i] = ct[i] - at[i];
-                    wt[i] = tat[i] - bt[i];
+                    time += rem_bt[i];
 
-                    total_wt += wt[i];
+                    wt[i] = time - bt[i];
 
-                    completed++;
+                    rem_bt[i] = 0;
                 }
 
-                printf("| P%d | %d ",i+1,time);
+                printf("%d ", time);
             }
         }
+
+    }while(done == 0);
+
+    float total_wt = 0;
+    float total_tat = 0;
+
+    for(int i=0;i<n;i++){
+
+        tat[i] = wt[i] + bt[i];
+
+        total_wt += wt[i];
+        total_tat += tat[i];
     }
 
     *avg_wt = total_wt / n;
+    *avg_tat = total_tat / n;
 
-    printf("\nAverage Waiting Time = %.2f\n",*avg_wt);
+    printf("\nAverage Waiting Time = %.2f\n", *avg_wt);
+    printf("Average Turnaround Time = %.2f\n", *avg_tat);
 }
