2fc6d6ec85b46cc4c6892143088ea6126d6afb8d
angelsachdeva3560-cmd <angelsachdeva3560@gmail.com>
2026-03-30 22:07:05 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,49 +1,40 @@
 #include <stdio.h>
 #include "scheduler.h"
 
-void round_robin(Process p[], int n, int time_quantum) {
-    int remaining[n];
-    int waiting_time[n], turnaround_time[n];
-    int time = 0, done;
+void roundRobin(int n, int at[], int bt[], int quantum, float *avg_wt) {
+    int rt[n], wt[n], time = 0, completed = 0;
 
-    // Initialize remaining burst times
     for (int i = 0; i < n; i++) {
-        remaining[i] = p[i].burst_time;
-        waiting_time[i] = 0;
+        rt[i] = bt[i];
+        wt[i] = 0;
     }
 
-    do {
-        done = 1;
+    while (completed < n) {
+        int done = 1;
 
         for (int i = 0; i < n; i++) {
-            if (remaining[i] > 0) {
+            if (rt[i] > 0 && at[i] <= time) {
                 done = 0;
 
-                if (remaining[i] > time_quantum) {
-                    time += time_quantum;
-                    remaining[i] -= time_quantum;
+                if (rt[i] > quantum) {
+                    time += quantum;
+                    rt[i] -= quantum;
                 } else {
-                    time += remaining[i];
-                    waiting_time[i] = time - p[i].burst_time;
-                    remaining[i] = 0;
+                    time += rt[i];
+                    wt[i] = time - bt[i] - at[i];
+                    rt[i] = 0;
+                    completed++;
                 }
             }
         }
-    } while (!done);
 
-    // Calculate turnaround time
-    for (int i = 0; i < n; i++) {
-        turnaround_time[i] = p[i].burst_time + waiting_time[i];
+        if (done)
+            time++;
     }
 
-    // Print results
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    float total_wt = 0;
+    for (int i = 0; i < n; i++)
+        total_wt += wt[i];
 
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\n",
-               p[i].pid,
-               p[i].burst_time,
-               waiting_time[i],
-               turnaround_time[i]);
-    }
+    *avg_wt = total_wt / n;
 }
