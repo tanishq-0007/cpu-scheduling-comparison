6c245d9983ae46a4dcd88f756abf42680645be17
Tanishq Singh <96950310+tanishq-0007@users.noreply.github.com>
2026-03-30 23:00:28 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,37 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "scheduler.h"
 
-void fcfs(int n, int at[], int bt[], float *avg_wt)
-{
-    int ct[n], tat[n], wt[n];
+void fcfs(int n, int at[], int bt[], float *avg_wt, float *avg_tat){
 
-    ct[0] = at[0] + bt[0];
+    int wt[n], tat[n];
 
-    for(int i=1;i<n;i++)
-    {
-        if(ct[i-1] < at[i])
-            ct[i] = at[i] + bt[i];
-        else
-            ct[i] = ct[i-1] + bt[i];
+    wt[0] = 0;
+
+    for(int i=1;i<n;i++){
+        wt[i] = wt[i-1] + bt[i-1];
     }
 
     float total_wt = 0;
+    float total_tat = 0;
 
-    printf("\n--- FCFS ---\nGantt Chart:\n");
+    printf("Gantt Chart:\n0 ");
 
-    printf("%d ", at[0]);
+    for(int i=0;i<n;i++){
 
-    for(int i=0;i<n;i++)
-    {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
+        printf("| P%d | %d ", i+1, wt[i]+bt[i]);
+
+        tat[i] = wt[i] + bt[i];
 
         total_wt += wt[i];
-
-        printf("| P%d | %d ",i+1,ct[i]);
+        total_tat += tat[i];
     }
 
     *avg_wt = total_wt / n;
+    *avg_tat = total_tat / n;
 
-    printf("\nAverage Waiting Time = %.2f\n",*avg_wt);
+    printf("\nAverage Waiting Time = %.2f\n", *avg_wt);
+    printf("Average Turnaround Time = %.2f\n", *avg_tat);
 }
